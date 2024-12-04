7256c637439ade462799721cbdd6ce1f7398b167
MilanMaPangPang <120579337+MilanMaPangPang@users.noreply.github.com>
2024-12-04 10:45:39 +0100

    making theme dynamic

--- themes/schooltheme/functions.php
+++ themes/schooltheme/functions.php
@@ -1,6 +1,16 @@
 <?php
-function firstTheme_style(){
-    wp_enqueue_style( 'core', 'style.css', false ); 
+function firstTheme_style()
+{
+    wp_enqueue_style('core', 'style.css', false);
 }
 add_action('wp_enqueue_scripts', 'firstTheme_style');
+
+function mijn_menu_registreren()
+{
+    register_nav_menus(array(
+        'links_menu' => __('Links Menu', 'mijn_thema'),
+        'footer_menu' => __('Footer Menu', 'mijn_thema'),
+    ));
+}
+add_action('init', 'mijn_menu_registreren');
 ?>
