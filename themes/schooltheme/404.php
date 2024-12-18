291a8136ef2a849f667e8ea93fb3aefe830f5c87
MilanMaPangPang <120579337+MilanMaPangPang@users.noreply.github.com>
2024-12-18 11:27:48 +0100

    added 404 error page

---
+++ themes/schooltheme/404.php
@@ -0,0 +1,7 @@
+<section id="content">
+    <h1>Pagina niet gevonden</h1>
+    <p>De pagina die je zocht bestaat niet (meer).</p>
+    <p><a href="<?php echo home_url(); ?>">Terug naar de homepage</a></p>
+</section>
+
+
